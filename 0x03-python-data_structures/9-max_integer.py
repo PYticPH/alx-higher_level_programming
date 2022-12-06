1083b84dc7f7219c1613f3c4471bb1f4bc589035
PYticPH <pyticph@gmail.com>
2022-12-06 13:31:06 +0000

    Solved: task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+    maxNum = 0
+    for num in my_list:
+        if num >= maxNum:
+            maxNum = num
+    return (maxNum)
