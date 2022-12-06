080291ccc3df5de9ccaf8e46ec2276eb229f4492
PYticPH <pyticph@gmail.com>
2022-12-06 14:11:29 +0000

    Solved: task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def  divisible_by_2(my_list=[]):
+    if not my_list:
+        return (False)
+    new_list = []
+    for num in my_list:
+        new_list.append(bool(num % 2 == 0))
+    return (new_list)
