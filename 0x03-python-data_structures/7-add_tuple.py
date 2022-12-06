464e7a32ddd234cf82e724e5cd629ce804faf86f
PYticPH <pyticph@gmail.com>
2022-12-06 12:58:23 +0000

    Fix: output error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum = ()
-    pad = (0,)
-    if not tuple_a:
-        return (tuple_b)
-    if not tuple_b:
-        return (tuple_a)
-    if len(tuple_a) < 2:
-        tuple_a += pad
-    elif len(tuple_b) < 2:
-        tuple_b += pad
-    sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return (sum)
+    pad = (0,0)
+    tuple_a += pad
+    tuple_b += pad
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
