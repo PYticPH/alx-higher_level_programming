8adc6edaab0885d1b857cfbcc46269367760a6b9
PYticPH <pyticph@gmail.com>
2022-12-07 23:32:20 +0000

    Fix: output error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from functools import reduce
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    max_int = None
+    max_key, max_int = None, None
     for key, value in a_dictionary.items():
         if max_int == None or value > max_int:
             max_key, max_int = key, value
