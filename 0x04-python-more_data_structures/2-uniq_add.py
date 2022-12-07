03f728244e215de240f03f17c1f3b0d2c4112a21
PYticPH <pyticph@gmail.com>
2022-12-07 13:45:11 +0000

    Fix: output error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if not my_list:
-        return (my_list)
-    sum = 0
-    for num in set(my_list):
-        sum += num
-    return (sum)
+        pass
+    else:
+        return (sum(set(my_list)))
