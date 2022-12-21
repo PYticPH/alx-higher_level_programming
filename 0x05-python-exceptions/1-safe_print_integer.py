bc37f98c76ccdbb1072e1f6f2a183fe4029e35df
PYticPH <pyticph@gmail.com>
2022-12-21 13:54:48 +0000

    Fix: requirement fail #2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,6 @@ def safe_print_integer(value):
     # funcion to print integer value only
     try:
         print("{:d}".format(value))
-    except as is_int:
-        is_int = False
-        return (is_int)
+    except (NameError, TypeError, ValueError):
+        return (False)
     return (True)
