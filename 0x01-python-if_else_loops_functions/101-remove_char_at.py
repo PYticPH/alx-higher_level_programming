19efda1eff7cf2eea048dbca5567b80e102dca40
PYticPH <pyticph@gmail.com>
2023-01-01 01:07:58 +0000

    Solved: task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    """ Function to copy a string and remove a character at n position """
+
+    newStr = ""
+
+    for num in range(len(str)):
+        if (num != n):
+            newStr += str[num]
+    
+    return (newStr)
