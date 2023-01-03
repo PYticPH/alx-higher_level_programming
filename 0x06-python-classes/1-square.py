97f3ea6c4426e4e94229ef2502838568de6004f8
PYticPH <pyticph@gmail.com>
2023-01-03 08:51:02 +0000

    Solved: task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A class Square that defines a square """
+
+class Square:
+    """ class square """
+    def __init__(self, size):
+        """ initialize square with private attribute siize """
+        self.__size = size
+
