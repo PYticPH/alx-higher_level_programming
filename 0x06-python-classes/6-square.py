901997ff0da4cb61b38b3dbab9f5e4ad3d65a2d6
PYticPH <pyticph@gmail.com>
2023-01-04 08:54:16 -0800

    Fix: output error #7

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
-"""
-class Square that defines a square by: (based on 5-square.py)
-
-Private instance attribute: size:
-property def size(self): to retrieve it
-property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with the
-message size must be an integer
-if size is less than 0, raise a ValueError exception with the message
-size must be >= 0
-Private instance attribute: position:
-property def position(self): to retrieve it
-property setter def position(self, value): to set it:
-position must be a tuple of 2 positive integers, otherwise raise a TypeError
-exception with the message position must be a tuple of 2 positive integers
-Instantiation with optional size and optional position:
-def __init__(self, size=0, position=(0, 0)): Public instance method:
-def area(self): that returns the current square area Public instance method:
-def my_print(self): that prints in stdout the square with the character #:
-if size is equal to 0, print an empty line
-"""
-
+""" class Square """
 
 class Square:
     """ clas square """
@@ -67,7 +46,7 @@ class Square:
     @position.setter
     def position(self, value):
         """ set the value of position """
-        if (value is not tuple) | (len(value) != 2):
+        if (type(value) is not tuple) | (len(value) != 2):
             raise TypeError(
                     "position must be a tuple of 2 positive integers")
         elif (value[0] is not int) | (value[1] is not int):
